11da127fb00f11c790ed8c16ba4756b5f8c73f1e
aousabdo <aousabdo@gmail.com>
2014-05-08 14:29:23 -0400

    Update emailFilter.R

--- emailFilter.R
+++ emailFilter.R
@@ -1,3 +1,4 @@
+
 library(kernlab)
 
 ## Split the data set into two ~ equal in length sub data sets, one will be our 
@@ -22,3 +23,43 @@ plot(log10(trainSpam$capitalAve+1) ~ trainSpam$type)
 
 plot(log10(spam[, 1:4]+1))
 
+hCluster = hclust(dist(t(trainSpam[, 1:57])))
+plot(hCluster)
+
+hClusterUpdated = hclust(dist(t(log10(trainSpam[, 1:57] + 1))))
+plot(hClusterUpdated)
+
+## add new column which we will use as our logistic predictory
+trainSpam$numType <- as.numeric(trainSpam$type) -1
+
+costFunction <- function(x, y) sum(x != (y>0.5))
+cvError <- rep(NA, 55)
+
+library(boot)
+for (i in 1:55) {
+  ## Create formula for the logistic fit. The response in each case is the 
+  ## newly created numType binomial response
+  lmFormula = reformulate(names(trainSpam)[i], response = "numType")
+  ## fit a logistic model to each variable 
+  glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+  ## Get the cross validated error for each case
+  cvError[i] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
+}
+names(trainSpam)[which.min(cvError)]
+
+
+## Use the best model from the group
+predictionModel = glm(numType ~ charDollar, family = "binomial", data = trainSpam)
+
+## Get predictions on the test set
+predictionTest = predict(predictionModel, testSpam)
+predictedSpam = rep("nonspam", dim(testSpam)[1])
+
+## Classify as `spam' for those with prob > 0.5
+predictedSpam[predictionModel$fitted > 0.5] = "spam"
+
+## Classification table
+table(predictedSpam, testSpam$type)
+
+## Error rate 
+(61+458)/(61+458+1346+449)
